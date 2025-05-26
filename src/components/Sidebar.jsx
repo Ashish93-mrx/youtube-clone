9e3a1a62555895c5473db00f3e6a72fa80aa93ba
Ashish <ashishcoorg46@gmail.com>
2025-05-26 16:25:33 +0530

    added linkedin link

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,8 @@
-import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router";
 import { removeSearch } from "../utils/resultSlice";
 import { toggleMenu } from "../utils/appSlice";
-import { FaHome } from "react-icons/fa";
+import { FaHome, FaLinkedin } from "react-icons/fa";
 import { MdLiveTv } from "react-icons/md";
 
 const Sidebar = () => {
@@ -24,7 +23,6 @@ const Sidebar = () => {
   const menuItems = [
     { name: "Home", icon: <FaHome />, to: "/" },
     { name: "Live", icon: <MdLiveTv />, to: "/live" },
-    // { name: "Movies", icon: <MdLocalMovies /> },
   ];
 
   const exploreItems = [
@@ -66,6 +64,23 @@ const Sidebar = () => {
               )}
             </li>
           ))}
+          <li>
+            {" "}
+            <span className="flex items-center gap-4 p-2 rounded-lg hover:bg-gray-100 cursor-pointer dark:hover:bg-gray-700">
+              {" "}
+              <a
+                href="https://www.linkedin.com/in/ashish-n-m/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex items-center gap-4 w-full"
+              >
+                <span>
+                  <FaLinkedin />
+                </span>{" "}
+                {"Linkedin"}
+              </a>
+            </span>{" "}
+          </li>
         </ul>
 
         <h2 className="mt-4 mb-2 font-semibold">Explore</h2>
