aba860675e403a095b6606c2012757df8d97303c
Ashish N M <76056451+Ashish93-mrx@users.noreply.github.com>
2025-05-26 09:52:00 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,5 @@
-export const GOOGLE_API_KEY = "AIzaSyCQFDaM_j77RtTB26RGIE4YofQN_3SSqfI";
+// export const GOOGLE_API_KEY = "AIzaSyCQFDaM_j77RtTB26RGIE4YofQN_3SSqfI";
+export const GOOGLE_API_KEY = "AIzaSyDYNgKsjEYy3La_Lvgaao9JWccwkylmlII";
 
 export const YOUTUBE_VIDEO_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
