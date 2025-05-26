47b5a70f0449d69942436039568bdb0106947dfc
Ashish N M <76056451+Ashish93-mrx@users.noreply.github.com>
2025-05-26 09:56:55 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -9,6 +9,8 @@ export const YOUTUBE_LIVE_VIDEO_API =
   "https://www.googleapis.com/youtube/v3/search?part=snippet&eventType=live&type=video&maxResults=50&regionCode=IN&key=" +
   GOOGLE_API_KEY;
 
+export const YOUTUBE_WATCHPAGE_SUGGEST = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=40&regionCode=IN&videoCategoryId=1&key="+GOOGLE_API_KEY;
+
 export const YOUTUBE_SEARCH_SUGGESTION_API =
   "https://ash-yt-backend-vaz5.vercel.app/suggest?q=";
 
